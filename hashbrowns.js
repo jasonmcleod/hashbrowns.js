dfe3cbd8247eab2d714c6076dd081879f065f68b
Jason McLeod <pixelparty@gmail.com>
2010-07-30 10:29:24 -0400

    renamed vars to be more fitting, and exposed them (hash.prev, hash.next now available)

--- hashbrowns.js
+++ hashbrowns.js
@@ -1,23 +1,23 @@
-hash = {running:false,rules:[],prevhash:"",beforeNext:0};
-hash.go = function(h) {
+hash = {now:"",running:false,rules:[],prev:"",next:"",beforeNext:0};
+hash.set = function(h) {
 	window.location.hash = h;
 }
 hash.init = function() {
-	window.location.hash = "";
+	hash.running = true;
 	setInterval(function() {
-		realhash = window.location.hash.replace("#","");
-		if(hash.prevhash != realhash) {						
+		hash.now = window.location.hash.replace("#","");
+		if(hash.prev != hash.now) {						
 			if(typeof hash.beforeNext == "function") {
 				hash.beforeNext.call();						
 			}
-			if(typeof hash.rules[realhash].onEnter == "function") {
-				hash.rules[realhash].onEnter.call();
-				hash.beforeNext = hash.rules[realhash].onExit;
-				hash.prevhash = realhash;
+			if(typeof hash.rules[hash.now] == "object" && typeof hash.rules[hash.now].onEnter == "function") {
+				hash.rules[hash.now].onEnter.call();
+				hash.beforeNext = hash.rules[hash.now].onExit;
+				hash.prev = hash.now;
 			}
 		}
 	},100);
-}
+};
 hash.rule = function(r,i,o) {
 	hash.rules[r] = {};
 	hash.rules[r].onEnter = i;
